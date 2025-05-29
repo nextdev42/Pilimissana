8bbf5665422d3503059aaf69a5458dbb7cd79863
nextdev42 <jonathan.mrutu@protonmail.com>
2025-05-29 18:59:16 +0300

    Create about Two pages.js

---
+++ src/components/about/about Two pages.js
@@ -0,0 +1,50 @@
+import React from "react"
+import Fade from "react-reveal/Fade"
+
+const WorkTwo = () => {
+  return (
+    <div className="max-w-7xl mx-auto mt-10">
+      {/* <Fade duration={2200}>
+        <div className="flex flex-row justify-space xxs:flex-col xs:flex-col sm:flex-row mb-5 max-h-80">
+          <div className="w-2/4 flex flex-col justify-center bg-gradient-to-r from-pink to-purple p-8 rounded-xl m-5 xxs:w-full xs:w-full sm:w-full lg:w-full">
+            <h1 className="text-5xl mt-5 font-bold text-white">
+              test<br></br>
+            </h1>
+            <p className="mt-5 text-white opacity-70">
+              test
+            </p>
+          </div>
+          <div className="overflow-hidden rounded-xl m-5 xxs:w-full xs:w-full sm:w-4/6 object-cover object-center h-full w-full">
+            <img src={Image3} alt="image"></img>
+          </div>
+        </div>
+      </Fade> */}
+
+      
+
+      <div className="mt-10 px-8">
+        <h2 className="text-black text-4xl font-semibold">Timu Yetu</h2>
+        <h3 className="text-black text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">
+          (Our Team)
+        </h3>
+         
+        <p className="text-black opacity-70 font-normal mt-3 text-xs xxs:text-xs xs:text-xs sm:text-xs md:text-sm lg:text-lg">
+       Katika Pilimisanah Foundation, tuna timu yenye kujitolea inayojumuisha:
+
+Wataalamu wa ushauri nasaha na afya ya akili wenye uzoefu wa kushughulika na matatizo ya uraibu
+
+Wale waliowahi kuwa waraibu waliopona na sasa wanatumia uzoefu wao kuwasaidia wengine
+
+Waratibu wa programu na wasimamizi wa kila kituo
+
+Wafanyakazi wa msaada wanaosaidia kuhakikisha vituo vyetu vinafanya kazi kwa usalama na kwa ufanisi
+
+
+Kila mwanatimu ameunganishwa na lengo moja kuu: kutembea na mteja katika safari ya kupona kwa heshima, upendo na matumaini. 
+       </p>
+      </div>
+    </div>
+  )
+}
+
+export default WorkTwo
