618b8b7911870d5517010fe5c7e98bbf8e2f77c2
nextdev42 <jonathan.mrutu@protonmail.com>
2025-06-02 19:55:14 +0300

    Update aboutGrid.js

--- src/components/about/aboutGrid.js
+++ src/components/about/aboutGrid.js
@@ -6,11 +6,11 @@ const AboutGrid = () => {
     <div className="max-w-7xl mx-auto px-4 my-20 grid grid-cols-3 gap-16 justify-items-center xxs:grid-cols-1 sm:grid-cols-3">
       <AboutTeamCard
         imgSrc="https://i.postimg.cc/25QCt4TX/Polish-20250602-192514223.jpg"
-        position="Holographic Liquid"
+        position="Waraibu waliopona Pata nafuu"
       ></AboutTeamCard>
       <AboutTeamCard
-        imgSrc="https://i.postimg.cc/WbBzjWWc/Holographic-Liquid2.webp"
-        position="Holographic Liquid"
+        imgSrc="https://i.postimg.cc/NfVYjgMt/Polish-20250602-194858542.jpg"
+        position="Vyakula kama sehemu ya tiba"
       ></AboutTeamCard>
       <AboutTeamCard
         imgSrc="https://i.postimg.cc/907FTc4F/Holographic-Liquid1.webp"
