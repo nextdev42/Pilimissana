f43f5a9803baed4abf2d35575ea8e16dc84d4f42
nextdev42 <jonathan.mrutu@protonmail.com>
2025-06-02 20:45:25 +0300

    Update featureSection.js

--- src/components/Home/featureSection.js
+++ src/components/Home/featureSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 
 // Assets
-import HeroImage from "../../images/Polish_20250602_203153731.jpg";
+import HeroImage from "../../images/Polish_20250602_203951067.jpg";
 import Image1 from "../../images/Polish_20250602_155142242.jpg";
 import Image2 from "../../images/Polish_20250602_160038191.jpg";
 import Image3 from "../../images/3D-liquid-abstract-3.webp";
