a67e24579dba3e892218bea8bc7cdb81a60ae57d
nextdev42 <jonathan.mrutu@protonmail.com>
2025-06-02 20:06:41 +0300

    Update aboutGrid.js

--- src/components/about/aboutGrid.js
+++ src/components/about/aboutGrid.js
@@ -6,15 +6,15 @@ const AboutGrid = () => {
     <div className="max-w-7xl mx-auto px-4 my-20 grid grid-cols-3 gap-16 justify-items-center xxs:grid-cols-1 sm:grid-cols-3">
       <AboutTeamCard
         imgSrc="https://i.postimg.cc/25QCt4TX/Polish-20250602-192514223.jpg"
-        position="Waraibu waliopona Pata nafuu"
+        position="Waraibu waliopona"
       ></AboutTeamCard>
       <AboutTeamCard
         imgSrc="https://i.postimg.cc/NfVYjgMt/Polish-20250602-194858542.jpg"
         position="Vyakula kama sehemu ya tiba"
       ></AboutTeamCard>
       <AboutTeamCard
-        imgSrc="https://i.postimg.cc/907FTc4F/Holographic-Liquid1.webp"
-        position="Holographic Liquid"
+        imgSrc="https://i.postimg.cc/ZnWbw0Rf/Polish-20250602-200058560.jpg"
+        position="Kazi Mbali Mbali za mikono kama sehemu ya tiba"
       ></AboutTeamCard>
     </div>
   )
