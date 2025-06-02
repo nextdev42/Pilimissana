b96774d4aca2d2c2176f6547bc6bf1a561984124
nextdev42 <jonathan.mrutu@protonmail.com>
2025-06-02 19:41:06 +0300

    Update aboutGrid.js

--- src/components/about/aboutGrid.js
+++ src/components/about/aboutGrid.js
@@ -5,7 +5,7 @@ const AboutGrid = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 my-20 grid grid-cols-3 gap-16 justify-items-center xxs:grid-cols-1 sm:grid-cols-3">
       <AboutTeamCard
-        imgSrc="https://i.postimg.cc/HWhyyLNH/Holographic-Liquid3.webp"
+        imgSrc="https://i.postimg.cc/25QCt4TX/Polish-20250602-192514223.jpg"
         position="Holographic Liquid"
       ></AboutTeamCard>
       <AboutTeamCard
