2f63bfe2d826382fc22f1ce75563a1a9b0f16fe3
nextdev42 <jonathan.mrutu@protonmail.com>
2025-06-02 16:04:48 +0300

    Update featureSection.js

--- src/components/Home/featureSection.js
+++ src/components/Home/featureSection.js
@@ -4,7 +4,7 @@ import Fade from "react-reveal/Fade";
 // Assets
 import HeroImage from "../../images/Polish_20250602_152434075.jpg";
 import Image1 from "../../images/Polish_20250602_155142242.jpg";
-import Image2 from "../../images/3D-liquid-abstract.webp";
+import Image2 from "../../images/Polish_20250602_160038191.jpg";
 import Image3 from "../../images/3D-liquid-abstract-3.webp";
 
 const FeatureSection = () => {
