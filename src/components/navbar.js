38a98ddddd18d16a2125c644594b1d5fbd10e7d3
nextdev42 <jonathan.mrutu@protonmail.com>
2025-06-09 12:33:13 +0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
                 </Link>
                 <div className="items-center">
                   <a
-                    href="tel:#"
+                    href="tel:+255715744464 "
                     className="transition-all duration-500ms ease-in-out hover:ease-in-out bg-transparent mt-5 py-2.5 px-4 text-base font-medium text-center text-black rounded-lg border border-purple hover:text-white border-black  hover:bg-purple"
                   >
                     Tupigie Sasa
