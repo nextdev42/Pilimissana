2a05474cb57d60d1bd66a6eb119ed427aa57d85a
nextdev42 <jonathan.mrutu@protonmail.com>
2025-06-19 18:20:08 +0300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,12 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
 
-  // const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  return graphql(
-    `
+  const result = await graphql(`
     {
-      allMarkdownRemark(limit: 1000, sort: {frontmatter: {date: DESC}}) {
+      allMarkdownRemark(limit: 1000, sort: { frontmatter: { date: DESC } }) {
         edges {
           node {
             id
@@ -19,49 +17,47 @@ exports.createPages = ({ graphql, actions }) => {
               templateKey
               title
               date(formatString: "DD:MM:YYYY hh:mm")
-
             }
           }
         }
       }
     }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-    const posts = result.data.allMarkdownRemark.edges
-    // Template For blog-post
-    const blogPost = posts.filter(item => item.node.frontmatter.templateKey === 'blog-post')
-    blogPost.forEach((post, index) => {
-      const previous = index === blogPost.length - 1 ? null : blogPost[index + 1].node
-      const next = index === 0 ? null : blogPost[index - 1].node
+  `);
 
-      createPage({
-        // path: post.node.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' : `/${post.node.fields.slug.split('/').slice(2, -1).join('/')}`,
-        path: post.node.fields.slug,
-        component: path.resolve(
-          `src/templates/blog-post.js`
-        ),
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-  
-    return null
-  })
-}
+  if (result.errors) throw result.errors;
+
+  const posts = result.data.allMarkdownRemark.edges;
+
+  const blogPost = posts.filter(
+    item => item.node.frontmatter.templateKey === "blog-post"
+  );
+
+  blogPost.forEach((post, index) => {
+    const previous =
+      index === blogPost.length - 1 ? null : blogPost[index + 1].node;
+    const next = index === 0 ? null : blogPost[index - 1].node;
+
+    createPage({
+      path: post.node.fields.slug,
+      component: path.resolve(`src/templates/blog-post.js`),
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    });
+  });
+};
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
+
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode });
     createNodeField({
       name: `slug`,
       node,
       value,
-    })
+    });
   }
-}
+};
