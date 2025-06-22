0216be65eb7b8412a661000f9a6342b8863fce44
nextdev42 <jonathan.mrutu@protonmail.com>
2025-06-22 13:43:39 +0300

    Update blogHeader.js

--- src/components/Blog/blogHeader.js
+++ src/components/Blog/blogHeader.js
@@ -1,11 +1,13 @@
+import React from "react"
 import { Link } from "gatsby"
 import { getImage, GatsbyImage } from "gatsby-plugin-image"
-import React from "react"
 import Fade from "react-reveal/Fade"
 import { marked } from "marked"
 
 const BlogHeader = ({ post }) => {
-  const image = getImage(post.node.frontmatter.featuredimage?.childImageSharp)
+  const featuredImage = getImage(post.node.frontmatter.featuredimage?.childImageSharp)
+  const title = post.node.frontmatter.title
+  const slug = post.node.fields.slug
 
   const rawDescription = post.node.frontmatter.description || ""
   const htmlDescription = marked.parseInline(rawDescription)
@@ -16,12 +18,11 @@ const BlogHeader = ({ post }) => {
   return (
     <div className="max-w-7xl xxs:mx-4 sm:mx-auto mt-10">
       <Fade duration={2200}>
-        <div className="flex flex-row justify-space xxs:flex-col-reverse xs:flex-col-reverse sm:flex-row mb-5">
+        <div className="flex flex-row justify-space xxs:flex-col-reverse sm:flex-row mb-5">
+          {/* Text content */}
           <div className="max-h-80 w-11/12 sm:mx-5 xxs:my-5 flex flex-col justify-center bg-gradient-to-r from-pink to-purple p-8 rounded-xl lg:m-5 w-full">
-            <Link to={post.node.fields.slug} style={{ textDecoration: "none" }}>
-              <h1 className="text-white text-2xl font-semibold">
-                {post.node.frontmatter.title}
-              </h1>
+            <Link to={slug} style={{ textDecoration: "none" }}>
+              <h1 className="text-white text-2xl font-semibold">{title}</h1>
             </Link>
             <p
               className="text-lg mt-4 text-white opacity-70 xxs:text-sm sm:text-lg"
@@ -29,15 +30,20 @@ const BlogHeader = ({ post }) => {
             />
           </div>
 
-          <div className="my-5 mr-5 w-full overflow-hidden rounded-xl">
-            {image && (
-              <Link to={post.node.fields.slug} style={{ textDecoration: "none", color: "black" }}>
+          {/* Image content */}
+          {featuredImage && (
+            <div className="my-5 mr-5 w-full overflow-hidden rounded-xl">
+              <Link to={slug} style={{ textDecoration: "none", color: "black" }}>
                 <div className="max-h-80 w-auto h-full object-cover flex justify-center">
-                  <GatsbyImage image={image} alt={post.node.frontmatter.title} className="img" />
+                  <GatsbyImage
+                    image={featuredImage}
+                    alt={title}
+                    className="rounded-xl w-full h-full object-cover"
+                  />
                 </div>
               </Link>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       </Fade>
     </div>
